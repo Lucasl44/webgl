954518feeb48d2f4b64e96dce0aa23154d45715d
Lucas James Lederman <lucas.lederman@aveva.com>
2023-03-22 14:20:17 +0000

    Made it take any custom colors for fill rect

--- src/canvas2d.js
+++ src/canvas2d.js
@@ -16,18 +16,18 @@ function calculatePixelIndices(top, left, width, height) {
   return pixelIndices;
 }
 
-function fillRect(top, left, width, height) {
+function fillRect(top, left, width, height, r, g, b, a) {
   const pixelStore = new Uint8ClampedArray(canvas.width * canvas.height * 4);
   const pixelIndices = calculatePixelIndices(top, left, width, height);
   pixelIndices.forEach((i) => {
-    pixelStore[i] = 0; //r
-    pixelStore[i + 1] = 0 //g
-    pixelStore[i + 2] = 0 //b
-    pixelStore[i + 3] = 255 //alpha
+    pixelStore[i] = r; //r
+    pixelStore[i + 1] = g //g
+    pixelStore[i + 2] = b //b
+    pixelStore[i + 3] = a//alpha
 });
 
   const imageData = new ImageData(pixelStore, canvas.width, canvas.height);
   ctx.putImageData(imageData, 0, 0);
 }
 
-fillRect(10,10,100,50);
+fillRect(10,10,100,50, 50, 100, 200, 255);
