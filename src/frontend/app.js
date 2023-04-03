45f5b2ce6b1360e91f2607b2d9e7eb9b967958d6
Lucas James Lederman <lucas.lederman@aveva.com>
2023-04-03 15:17:23 +0100

    circle

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { shader } from './archive/shader.js';
-import { shaderHexagon } from './shaderTriangle.js';
+import { shaderCircle } from './shaderTriangle.js';
 
 export const App = () => {
   const [el, setEl] = useState(null);
@@ -8,7 +8,7 @@ export const App = () => {
   };
 
   useEffect(() => {
-    shaderHexagon(el)
+    shaderCircle(el)
   }, [el]);
 
   return (
